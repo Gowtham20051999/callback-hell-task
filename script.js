02cde02d38bbd8c415e3363e1b6541069fcd3892
Gowtham20051999 <83176312+Gowtham20051999@users.noreply.github.com>
2022-09-24 11:10:40 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let container= document.createElement("div");
+container.setAttribute("class","container");
+let span=document.getElementById("span")
+
+span.innerHTML=10;
+document.body.append(container);
+let time=10
+setTimeout(()=>{
+    time=time-1
+    span.innerHTML=time
+    setTimeout(()=>{
+        time=time-1
+        span.innerHTML=time
+        setTimeout(()=>{
+            time=time-1
+            span.innerHTML=time
+            setTimeout(()=>{
+                time=time-1
+                span.innerHTML=time
+                setTimeout(()=>{
+                    time=time-1
+                    span.innerHTML=time
+                    setTimeout(()=>{
+                        time=time-1
+                        span.innerHTML=time
+                        setTimeout(()=>{
+                            time=time-1
+                            span.innerHTML=time
+                            setTimeout(()=>{
+                                time=time-1
+                                span.innerHTML=time
+                                setTimeout(()=>{
+                                    time=time-1
+                                    span.innerHTML=time
+                                    setTimeout(()=>{
+                                        time=time-1
+                                        span.innerHTML="😘😍❤Happy Birthday❤😍😘"
+                                },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+
+            },1000)
+        },1000)
+
+    },1000)
+},1000)
